0cc7336049b8eb8fc19fd987bbc3ff7fb88941ed
Konrad <231018054+K-SCode@users.noreply.github.com>
2026-05-18 00:29:30 +0200

    feat: Add repository intefaces

---
+++ LibraryOnline.Core/Interfaces/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using LibraryOnline.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryOnline.Core.Interfaces
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        Task AddAsync(T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(Guid id);
+        Task DeleteAsync(T entity);
+        Task<bool> ExistsAsync(Guid id);
+    }
+}
---
+++ LibraryOnline.Core/Interfaces/IRoleRepository.cs
@@ -0,0 +1,11 @@
+﻿using LibraryOnline.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryOnline.Core.Interfaces
+{
+    public interface IRoleRepository : IBaseRepository<Role>
+    {
+    }
+}
