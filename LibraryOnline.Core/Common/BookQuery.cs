475707f48977613ee3c06ee4c1cf254dd7ad462b
Konrad <231018054+K-SCode@users.noreply.github.com>
2026-05-20 15:55:57 +0200

    feat: added BookQuery and changed structure of IBookRepository

---
+++ LibraryOnline.Core/Common/BookQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryOnline.Core.Common
+{
+    public class BookQuery
+    {
+        public string? Search { get; set; }
+        public Guid? CategoryId { get; set; }
+        public Guid? AuthorId { get; set; }
+        public string? Sort { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+    }
+}
--- LibraryOnline.Core/Interfaces/Repository/IBookRepository.cs
+++ LibraryOnline.Core/Interfaces/Repository/IBookRepository.cs
@@ -1,11 +1,11 @@
-﻿using LibraryOnline.Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using LibraryOnline.Core.Common;
+using LibraryOnline.Core.Entities;
 
 namespace LibraryOnline.Core.Interfaces.Repository
 {
     public interface IBookRepository : IRepository<Book>
     {
+        Task<IEnumerable<Book>> GetAllAsync(BookQuery query);
+        
     }
 }
