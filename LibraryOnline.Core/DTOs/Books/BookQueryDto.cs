be4fee4ea6af85aa43cc0a852730b4d6c673a38d
Konrad <231018054+K-SCode@users.noreply.github.com>
2026-05-20 14:59:09 +0200

    feat: Add BookQueryDto

---
+++ LibraryOnline.Core/DTOs/Books/BookQueryDto.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryOnline.Core.DTOs.Books
+{
+    public class BookQueryDto
+    {
+        public string? Search {  get; set; }
+        public Guid? CategoryId { get; set; }
+        public Guid? AuthorOd {  get; set; }
+        public string? Sort {  get; set; } 
+        public string? Page {  get; set; }
+        public string? PageSizE { get; set; }
+    }
+}
